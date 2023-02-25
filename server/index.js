9ee8a41379e658dbc7a118d2003512557f11a44f
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-02-26 00:58:48 +0530

    Add Auth.js -middleware

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
@@ -38,7 +39,7 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
-app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
 
 /* ROUTES */
 app.use("/auth", authRoutes);
