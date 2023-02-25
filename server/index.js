3be8922f75736ee58f1de40b9fabaf4403d9b5a1
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-02-26 00:59:14 +0530

    Import User Routes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
@@ -43,8 +44,8 @@ app.post("/auth/register", upload.single("picture"), register);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+
 app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
