7616f59bf1818d6ffdd3c5e99a93fff126c48769
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-03-06 18:28:21 +0530

    Import postRoutes  from routes -> posts.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
